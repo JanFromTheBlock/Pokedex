cbcd0ae0786f67e7ac936fdb9e0485fc95f87271
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2023-10-21 18:02:57 +0200

    finishing PokemonDetails

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 let currentPokemon;
+let currentColor;
+let typeColors = [
+    { type: "normal", color: "#C6C6A7" },
+    { type: "fire", color: "#FFAB60" },
+    { type: "water", color: "#9DB7F5" },
+    { type: "grass", color: "#A7DB8D" },
+    { type: "electric", color: "#FFD86F" },
+    { type: "ice", color: "#BCE2E8" },
+    { type: "fighting", color: "#D56723" },
+    { type: "poison", color: "#B084AC" },
+    { type: "ground", color: "#EBD69D" },
+    { type: "flying", color: "#C6B7F5" },
+    { type: "psychic", color: "#FA92B2" },
+    { type: "bug", color: "#C6D16E" },
+    { type: "rock", color: "#D1C17D" },
+    { type: "ghost", color: "#A292BC" },
+    { type: "dragon", color: "#A27DFA" },
+    { type: "dark", color: "#8D8D68" },
+    { type: "steel", color: "#D1D1E0" },
+    { type: "fairy", color: "#F4BDC9" }
+  ];
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log('Loaded pokemon', currentPokemon)
@@ -10,20 +31,33 @@ async function loadPokemon() {
 
 function renderPokemonInfo() {
     document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemon-pic').src = currentPokemon['sprites']['front_default'];
+    document.getElementById('pokemon-pic').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
     document.getElementById('poke-index').innerHTML = /*html*/`#00${currentPokemon['id']}`;
     document.getElementById('type1').innerHTML = currentPokemon['types']['0']['type']['name'];
     if (currentPokemon['types']['1']) {
         document.getElementById('type2').innerHTML = currentPokemon['types']['1']['type']['name'];
         document.getElementById('type2').classList.remove('d-none');
     }
+    getColorForType()
+    SetBackgroundColor();
+}
+
+function getColorForType(){
+    typeName = currentPokemon['types']['0']['type']['name'];
+    for (let i = 0; i < typeColors.length; i++) {
+        if (typeColors[i].type === typeName) {
+          currentColor = typeColors[i].color;
+        }
+      }
+}
+function SetBackgroundColor(){
+    document.getElementById('pokedex').style.background = currentColor;
 }
 function openAbout() {
     document.getElementById('about').classList.add('open-topic');
     document.getElementById('info-area').innerHTML = /*html*/`
         <div>
-            <h2>Overview</h2>
-           <table>
+           <table id="table-about">
            <tr>
                <td>Height</td>
                <td>${currentPokemon['height']}</td> 
@@ -50,5 +84,70 @@ function openAbout() {
         } else {
             document.getElementById('abilities').innerHTML += element;
         }
+    }
+}
+
+function openBaseStats() {
+    document.getElementById('base-stats').classList.add('open-topic');
+    document.getElementById('info-area').innerHTML = /*html*/`
+    <div>
+    <table id="table-base-stats">
+        <tr>
+            <td>HP</td>
+            <td>${currentPokemon['stats'][0]['base_stat']}</td>
+            <td><div class="progress-bar-outisde">
+                    <div class="progress-bar-inside" id="progress0"></div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Attack</td>
+            <td>${currentPokemon['stats'][1]['base_stat']}</td>
+            <td><div class="progress-bar-outisde">
+                    <div class="progress-bar-inside" id="progress1"></div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Defense</td>
+            <td>${currentPokemon['stats'][2]['base_stat']}</td>
+            <td><div class="progress-bar-outisde">
+            <div class="progress-bar-inside" id="progress2"></div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Special Attack</td>
+            <td>${currentPokemon['stats'][3]['base_stat']}</td>
+            <td><div class="progress-bar-outisde">
+            <div class="progress-bar-inside" id="progress3"></div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Special Defense</td>
+            <td>${currentPokemon['stats'][4]['base_stat']}</td>
+            <td><div class="progress-bar-outisde">
+            <div class="progress-bar-inside" id="progress4"></div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Speed</td>
+            <td>${currentPokemon['stats'][5]['base_stat']}</td>
+            <td><div class="progress-bar-outisde">
+            <div class="progress-bar-inside" id="progress5"></div>
+                </div>
+            </td>
+        </tr>
+    </table>
+    </div>
+        
+    `
+
+    for (let index = 0; index < 6; index++) {
+        const progress = currentPokemon['stats'][index]['base_stat'];
+        document.getElementById('progress' + index).style.width = `${progress}%`
+        document.getElementById('progress' + index).style.background = currentColor
     }
 }
